2a8ac36907c612649b7bf7b944463bbb51088aae
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-02 14:14:34 +0200

    init project

---
+++ data.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: '789987987987',
+    name: 'Simphonier',
+    description: 'Ideal para colocar en el baño',
+    image: 'https://eltallerdelmueble.es/wp-content/uploads/2016/08/d012b.jpg',
+    price: 12.99,
+    section: 'salon'
+  },
+  {
+    id: '789987987987',
+    name: 'Simphonier',
+    description: 'Ideal para colocar en el baño',
+    image: 'https://eltallerdelmueble.es/wp-content/uploads/2016/08/d012b.jpg',
+    price: 12.99,
+    section: 'salon'
+  },
+  {
+    id: '789987987987',
+    name: 'Simphonier',
+    description: 'Ideal para colocar en el baño',
+    image: 'https://eltallerdelmueble.es/wp-content/uploads/2016/08/d012b.jpg',
+    price: 12.99,
+    section: 'salon'
+  },
+  {
+    id: '789987987987',
+    name: 'Simphonier',
+    description: 'Ideal para colocar en el baño',
+    image: 'https://eltallerdelmueble.es/wp-content/uploads/2016/08/d012b.jpg',
+    price: 12.99,
+    section: 'salon'
+  },
+  {
+    id: '789987987987',
+    name: 'Simphonier',
+    description: 'Ideal para colocar en el baño',
+    image: 'https://eltallerdelmueble.es/wp-content/uploads/2016/08/d012b.jpg',
+    price: 12.99,
+    section: 'salon'
+  }
+];
+
+module.exports = products;
